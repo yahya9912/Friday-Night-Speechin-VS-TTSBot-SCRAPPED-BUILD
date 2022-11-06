3b4f0a45809461cfc680aa96c7d092fe85cbd94d
Snoni <890234527@rcsd121.org>
2022-11-06 00:33:43 -0400

    and why he broken 😹😹

--- ui/MainMenuState.hx
+++ ui/MainMenuState.hx
@@ -1,4 +1,8 @@
 
-function create() {
-
+function createPost() {
+    FlxG.mouse.useSystemCursor = true;
+    state.menuItems.y;
+    state.bg.scale.set(1,1);
+    //state.menuItems.x = 0;
+    //state.menuItems.menuItem.y = (state.i * 100) + state.offsetY;
 }
