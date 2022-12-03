bd691d88b0a9a4787a31e280a6ce0b3d4bb09ec2
Landon Shields <lchammer0701@gmail.com>
2022-12-02 17:16:01 -0700

    new and Better Opening stuff

--- ui/MainMenuState.hx
+++ ui/MainMenuState.hx
@@ -1,10 +1,7 @@
-
+//a
 function createPost() {
     FlxG.mouse.useSystemCursor = true;
-    state.menuItems.y;
-//    state.bg.scale.set(1,1);
-    state.bg.scale.x = 0.5;
-    state.bg.scale.y = 0.5;
-    //state.menuItems.x = 0;
-    //state.menuItems.menuItem.y = (state.i * 100) + state.offsetY;
+    state.bg.scale.set(0.95,0.95);
+    state.bg.y -= 100;
+    state.fallBackBG.color = 0x383a3d;
 }
