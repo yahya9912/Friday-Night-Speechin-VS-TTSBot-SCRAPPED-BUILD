d748f34dbfa12e47faf469dc9d14d3ba22d966e8
Snoni <890234527@rcsd121.org>
2022-12-03 12:45:31 -0500

    lag fix !!!

--- global_scripts/miss.hx
+++ global_scripts/miss.hx
@@ -2,14 +2,10 @@ var healthTxt = FlxText;
 function onPlayerHit(direction:Int) {
 
     PlayState.noteMiss();
-    health = health - 0.3;
+    health = health - 0.00000003;
+    healthTxt = new FlxText(0, Window.height * 0.65, 0, "");
+    healthTxt.text = "health:" + Std.int(health);
+    healthTxt.setFormat(Paths.font("sonic.ttf"), 24);
+    healthTxt.cameras = [camHUD];
+    PlayState.add(healthTxt);
 }
-
-function update(elapsed:Float)
-    {
-        healthTxt = new FlxText(0, Window.height * 0.65, 0, "");
-        healthTxt.text = "health:" + Std.int(health);
-        healthTxt.setFormat(Paths.font("sonic.ttf"), 24);
-        healthTxt.cameras = [camHUD];
-        PlayState.add(healthTxt);
-    }
