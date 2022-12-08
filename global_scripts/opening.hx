7f86b8ee69507d2718dce444026b255a1ccd3ec8
yahya9912 <yahya4mahdi@gmail.com>
2022-12-08 21:44:34 +0300

    fuck

--- global_scripts/opening.hx
+++ global_scripts/opening.hx
@@ -12,11 +12,7 @@ function create() {
 
 function onGuiPopup() {
     add(openers);
-<<<<<<< HEAD
-    FlxTween.tween(openers, {x: (FlxG.width / 1 - FlxG.width / 1)}, 1, {ease: FlxEase.quadInOut, onComplete: function () {
-=======
     FlxTween.tween(openers, {x: (FlxG.width / 2 - FlxG.width / 2) + 10}, 1, {ease: FlxEase.backInOut, onComplete: function () {
->>>>>>> dc43813b7049c20432c34b779d36aae433d682a5
         new FlxTimer().start(1, function(tmr:FlxTimer) {
             FlxTween.tween(openers, {x: (FlxG.width / 1 - FlxG.width / 1) - openers.width + 10}, 1, {ease: FlxEase.backIn});
         });
