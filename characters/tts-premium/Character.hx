126f9490c37bed0d12b4baf8550767c6d946c320
yahya9912 <yahya4mahdi@gmail.com>
2022-12-16 05:07:50 +0300

    That's a lot!
    get it because..... LJ....stuck.. a lot :trollface: ? uh okay imma stop

--- characters/tts-premium/Character.hx
+++ characters/tts-premium/Character.hx
@@ -1,4 +1,4 @@
 function create() {
 	character.frames = Paths.getCharacter(character.curCharacter);
-	character.loadJSON(true);
+	character.loadJSON(true); // Setting to true will override getColors() and dance().
 }
