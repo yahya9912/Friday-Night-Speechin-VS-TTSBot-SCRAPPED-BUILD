4a72b0e560fe6b7ca38682c7e56dc3be844508e4
alexg1247 <alexglawischnig@gmail.com>
2023-01-05 20:44:18 -0600

    added cam zoom for too slow thing

--- data/too slow remix/modchart.hx
+++ data/too slow remix/modchart.hx
@@ -3,6 +3,18 @@ function beatHit(curBeat){
         case 60:
             dad.switchCharacter("alexgs cat", mod);
             boyfriend.switchCharacter("tts-player", mod);
+        case 266:
+            defaultCamZoom += 0.2;
+        case 267:
+            defaultCamZoom += 0.2;
+        case 270:
+            defaultCamZoom -= 0.4;
+        case 282:
+            defaultCamZoom += 0.2;
+        case 283:
+            defaultCamZoom += 0.2;
+        case 286:
+            defaultCamZoom -= 0.4;
     }
        
 }
