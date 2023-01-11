496f0dde4f1284f473352745efe3d60b3a079190
Diamantitos2 <diamantitos002@gmail.com>
2023-01-11 13:32:08 -0600

    Fixes

--- scripts/monochrome.hx
+++ scripts/monochrome.hx
@@ -1,11 +1,7 @@
-import Sys;
 var health:FlxText;
 function create() {
     PlayState.boyfriend.visible = false;
     PlayState.gf.visible = false;
-    if (EngineSettings.botplay == true) {
-        Sys.exit(0);
-    }
     if (EngineSettings.middleScroll == false) {
         EngineSettings.middleScroll = true;
     }
