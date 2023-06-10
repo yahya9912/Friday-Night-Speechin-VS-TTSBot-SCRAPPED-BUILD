93ef5931236db9b7fbf5706c29415d491d38fdf6
eevbat <116406700+eevbat@users.noreply.github.com>
2023-06-10 14:53:22 -0400

    new icon bop

--- global_scripts/icon shmoove.hx
+++ global_scripts/icon shmoove.hx
@@ -1,6 +1,6 @@
 function updatePost(elapsed:Float) {
 //    if (Conductor.songPosition < 0) return;
     var ease:Float = FlxEase.circIn((Conductor.songPosition / Conductor.crochet) % 1);
-    PlayState.iconP1.angle = FlxMath.lerp(PlayState.curBeat % 2 == 0 ? 10 : -10, 0, ease);
-    PlayState.iconP2.angle = FlxMath.lerp(PlayState.curBeat % 2 == 0 ? -10 : 10, 0, ease);
+    PlayState.iconP1.angle = FlxMath.lerp(PlayState.curBeat % 2 == 0 ? 30 : -15, 0, ease);
+    PlayState.iconP2.angle = FlxMath.lerp(PlayState.curBeat % 2 == 0 ? -15 : 30, 0, ease);
 }
